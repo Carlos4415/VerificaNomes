06bd21d330819b569120ca888c1ef5f75748aaea
Carlos4415 <carlos.soares4415@gmail.com>
2025-05-31 23:02:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> nomesDosEscolhidos = new List<string>()
+{
+    "Bruce Wayne",
+    "Carlos Vilagran",
+    "Richard Grayson",
+    "Bob Kane",
+    "Will Farrel",
+    "Lois Lane",
+    "General Welling",
+    "Perla Letícia",
+    "Uxas",
+    "Diana Prince",
+    "Elisabeth Romanova",
+    "Anakin Wayne"
+};
+
+bool VerificaNomes(List<string> nomeDosEscolhidos, string escolhido)
+{
+    return nomesDosEscolhidos.Contains(escolhido);
+}
+
+if (VerificaNomes(nomesDosEscolhidos, "Carlos Vilagran"))
+{
+    Console.WriteLine("Nome existe na lista.");
+}
+else
+{
+    Console.WriteLine("Nome não existe na lista.");
+}
